c668ba8059d187f32210dfc0a7d07ca2f721665c
Anvesh Kartan <anveshkartan23@gmail.com>
2026-02-15 16:18:03 +0530

    catalogue

--- catalogue.sh
+++ catalogue.sh
@@ -8,6 +8,7 @@ N="\e[0m"
 
 LOGS_FOLDER="/var/log/shell-script"
 SCRIPT_NAME=$( echo $0 | cut -d "." -f1 )
+SCRIPT_DIR=$PWD
 MONGODB_HOST=mongodb.anveshdevops.fun
 LOG_FILE="$LOGS_FOLDER/$SCRIPT_NAME.log" # /var/log/shell-script/16-logs.log
 
