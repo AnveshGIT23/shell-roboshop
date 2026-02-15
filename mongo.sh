bf3504b393fbf23a111d7f1ccf7fe263d16f4ff6
Anvesh Kartan <anveshkartan23@gmail.com>
2026-02-15 15:35:34 +0530

    mongodb

--- mongo.sh
+++ mongo.sh
@@ -38,3 +38,10 @@ systemctl enable mongod &>>$LOG_FILE
 VALIDATE $? "Enabling MongoDB"
 
 systemctl start mongod
+VALIDATE $? "Start MongoDB"
+
+sed -i 's/127.0.0.1/0.0.0.0/g' /etc/mongod.conf
+VALIDATE $? "Allowing remote connections to MongoDB"
+
+systemctl restart mongod
+VALIDATE $? "Restared MongoDB"
